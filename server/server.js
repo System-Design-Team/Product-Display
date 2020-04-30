b0f763f32687aa4d9f4d1b7d0cbadcbb3b22e501
talltimblack <timothyrblack@gmail.com>
2020-04-30 15:20:24 -0500

    added keys to gitignore, added routes for fetching s3

--- server/server.js
+++ server/server.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const path = require('path');
 const port = 9001;
+const keys = require('./keys.js');
+const AWS = require('aws-sdk');
+const fs = require('fs');
 const queries = require('./database/queries');
 const db = require('./database/config');
 const app = express();
@@ -16,3 +19,39 @@ app.get("/", (req, res) => {
 app.listen(port, () => {
     console.log(`server is running and listening on port ${port}`);
 });
+
+//setting the credentials
+//The region should be the region of the bucket that you created
+//Visit this if you have any confusion - https://docs.aws.amazon.com/general/latest/gr/rande.html
+AWS.config.update({
+    accessKeyId: keys.iam_access_id,
+    secretAccessKey: keys.iam_secret,
+    region: 'us-east-2',
+});
+
+//Creating a new instance of S3:
+const s3 = new AWS.S3();
+
+
+//GET method route for downloading/retrieving file
+app.get('/get_file/:file_name',(req,res)=>{
+    retrieveFile(req.params.file_name, res);
+});
+
+//The retrieveFile function
+function retrieveFile(filename,res){
+
+const getParams = {
+    Bucket: 'worstboys-images-bucket',
+    Key: filename
+};
+
+s3.getObject(getParams, function(err, data) {
+    if (err){
+    return res.status(400).send({success:false,err:err});
+    }
+    else{
+    return res.send(data.Body);
+    }
+});
+}
