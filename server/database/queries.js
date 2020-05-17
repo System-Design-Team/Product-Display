2c42f2616d9e9ad209778ef6df1196cb00a7affe
talltimblack <timothyrblack@gmail.com>
2020-05-17 15:05:16 -0500

    updated for event listener search

--- server/database/queries.js
+++ server/database/queries.js
@@ -10,8 +10,8 @@ connection.connect((err) => {
     console.log('connected to db');
 });
 //function for getting product object via sku
-let getProduct = function(sku, callback) {
-    connection.query('SELECT * FROM products WHERE sku = ?',[sku], (err, results) => {
+let getProduct = function(name, callback) {
+    connection.query('SELECT * FROM products WHERE product_title = ?',[name], (err, results) => {
         //error checking from query
         if (err) {
             console.log('error querying db', err);
@@ -20,4 +20,16 @@ let getProduct = function(sku, callback) {
         callback(null, results);
     })
 }
+//function for getting product object via name
+// let getProductByName = function(name, callback) {
+//     connection.query('SELECT * FROM products WHERE sku = ?',[name], (err, results) => {
+//         //error checking from query
+//         if (err) {
+//             console.log('error querying db ', err);
+//             callback(err, null);
+//         }
+//         callback(null, results);
+//     })
+// }
 module.exports.getProduct = getProduct;
+// module.exports.getProductByName = getProductByName;
--- src/components/App.js
+++ src/components/App.js
@@ -8,25 +8,44 @@ export default class App extends Component {
     super(props);
     this.state = {
       doneFetching: false,
-      sku: 125613,
+      name: "Rugged Wrist Strap",
       productInfo: null
     };
     this.getData = this.getData.bind(this);
+    
+    // this.getDataByName = this.getDataByName.bind(this);
   }
 
   componentDidMount(){
-    this.getData()
+    this.getData();
+    console.log('hi from didMount');
+    document.addEventListener('submit', () => {
+      this.getSearchValue();
+    })
+    // this.getDataByName()
   }
-
+  getSearchValue() {
+    let search = document.getElementById('wilsoninputtag');
+    if(search){
+      let currentProduct = search.value;
+      console.log(currentProduct);
+      this.setState({
+        name: currentProduct
+      }, () => {
+        this.getData();
+      })
+    }
+  }
   //fetching product details function based off state saved sku
   getData() {
     axios.get('http://eb-cli-test-3-dev.us-east-2.elasticbeanstalk.com/view', {
       params: {
-        sku: this.state.sku
+        name: this.state.name
       }
     })
     //sets product details object in the state
     .then((productData) => {
+      console.log('hi from data fetch', productData);
       this.setState({
         productInfo: productData.data[0],
         doneFetching: true
@@ -36,7 +55,25 @@ export default class App extends Component {
       console.log('error fetching data', err);
     })
   }
+    //fetching product details function based off state saved name
 
+  // getDataByName() {
+  //   axios.get('http://eb-cli-test-3-dev.us-east-2.elasticbeanstalk.com/view/name', {
+  //     params: {
+  //       name: this.state.name
+  //     }
+  //   })
+  //   //sets product details object in the state
+  //   .then((productData) => {
+  //     this.setState({
+  //       productInfo: productData.data[0],
+  //       doneFetching: true
+  //     });
+  //   })
+  //   .catch(err => {
+  //     console.log('error fetching data', err);
+  //   })
+  // }
   render() {
     return (
       <div>
@@ -46,3 +83,11 @@ export default class App extends Component {
     );
   }
 }
+
+// window.addEventListener('click', (event) => {
+//   if (event.target.getAttribute('data-id') && event.target.getAttribute('data-id') !== this.state.productId && !isNaN(event.target.getAttribute('data-id'))) {
+//     this.setState({productId: event.target.getAttribute('data-id') }, () => {
+//       this.getRequest();
+//     });
+//   }
+// });
